da16461c9384e524a705673fa12d2674d89f442f
krigga <krigga7@gmail.com>
2022-12-20 21:34:32 +0300

    func version 0.2.0

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export type FuncWASMObject = {
+    schemaVersion: 1
+    funcVersion: string
+    module: any
+    wasmBase64: string
+};
+
+export const object: FuncWASMObject = {
+    schemaVersion: 1,
+    funcVersion: '0.2.0',
+    module: require('./funcfiftlib.js'),
+    wasmBase64: require('./funcfiftlib.wasm.js').FuncFiftLibWasm,
+};
