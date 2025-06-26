c7e23074e51f6524807b2043e54d479092e8ef14
krigga <25533192+krigga@users.noreply.github.com>
2025-06-26 16:00:02 +0300

    feat: debugger support (#4)

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ export type FuncWASMObject = {
     funcVersion: string
     module: any
     wasmBase64: string
+    debugger: {
+        module: any
+        gzipPatchBase64: string
+    }
 };
 
 export const object: FuncWASMObject = {
@@ -10,4 +14,8 @@ export const object: FuncWASMObject = {
     funcVersion: '0.4.6',
     module: require('./funcfiftlib.js'),
     wasmBase64: require('./funcfiftlib.wasm.js').FuncFiftLibWasm,
+    debugger: {
+        module: require('./funcfiftlib.debugger.js'),
+        gzipPatchBase64: require('./funcfiftlib.debugger.bpatch.gzip.js').DebuggerPatchGzip,
+    }
 };
